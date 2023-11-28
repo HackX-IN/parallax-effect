dc148ae76f52fbef5d59a6cc4f48e001923c7f95
ShivilaDev <138901497+shivillatech1@users.noreply.github.com>
2023-11-28 16:29:06 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import "react-native-gesture-handler";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;

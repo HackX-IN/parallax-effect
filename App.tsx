d6fd8a6005124981b735b5c3af3f53807c6e2209
ShivilaDev <138901497+shivillatech1@users.noreply.github.com>
2023-11-28 16:34:12 +0530

    navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import "react-native-gesture-handler";
-import { View, Text } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "@/src/screens/HomeScreen";
+import SingleScreen from "@/src/screens/id";
+import { StatusBar, useColorScheme } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const theme = useColorScheme();
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={theme === "dark" ? "white" : "black"} />
+      <NavigationContainer theme={theme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="id" component={SingleScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
